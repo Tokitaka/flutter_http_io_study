cc891e2a6dc53c12d703f1d022ab953109111c57
Juhye O <artigen3@gmail.com>
2023-04-03 15:13:51 +0900

    downloading data & rendering

--- lib/post_repository.dart
+++ lib/post_repository.dart
@@ -1,16 +1,28 @@
 
 import 'package:dio/dio.dart';
 
-class PostRepository {
-  final dio = Dio();
+import 'post.dart';
 
-  void findByIdV1 (int id){
-    print("1");
+class PostRepository {
+  final dio = Dio(BaseOptions(contentType: Headers.jsonContentType));
+
+  Future<Post> findById (int id) async{ // await - async 비동기니까 Future 박스를 줘야 해 Future 로 감싸기
     // Response 빈박스 돌려주는 것 - return 타입이 void 여도 그냥 future 박스에 데이터를 넣어주는 개념 ?
-    Future<Response> responseFT = dio.get("https://jsonplaceholder.typicode.com/posts/$id");
-    print("1");
-    responseFT.then((value) => print(value.data));
-    print("1");// 다운이 다되면 value 에 값 들어옴
+    Response response = await dio.get("https://jsonplaceholder.typicode.com/posts/$id");
+    Post post = Post.fromJson(response.data); // map -> dto 객체로 변환
+    return post;
+    // Map<String, dynamic> responseMap = response.data; //<key, value>
+    // print(responseMap["userId"]); // 1
+    // print(response.data);
+    // 단위테스트
+
+  }
+  Future<List<Post>> findAll () async{
+    // Response 빈박스 돌려주는 것 - return 타입이 void 여도 그냥 future 박스에 데이터를 넣어주는 개념 ?
+    Response response = await dio.get("https://jsonplaceholder.typicode.com/posts");
+    List<Map<String, dynamic>> mapList = response.data;
+    return mapList.map((e) => Post.fromJson(e)).toList();
+    print(response.data); // Json데이터 -> flutter의 map 타입으로 자동 변환해서 받는다 -> 오브젝트로 바꾸기만 하면됨
 
     // 단위테스트
 
--- test/post_repository_test.dart
+++ test/post_repository_test.dart
@@ -1,8 +1,29 @@
 // dart Test code
 import 'package:http_io_app/post_repository.dart';
 
-void main(){
+void main() async{
+  // await findById_test(); // await로 기다려주려면 Future<void> 가 되야 한다
+   // 실행이 종료될 때까지 기다리게 하기
+  await findById_test();
+
+}
+
+Future<void> findById_test() async{
+  //given
+  int id = 1;
+  //when
   PostRepository postRepository = PostRepository();
-  postRepository.findByIdV1(1);
+  await postRepository.findById(id);
+  //then
+
+}
+
+Future<void> findAll_test() async{
+  //given
+
+  //when
+  PostRepository postRepository = PostRepository();
+  await postRepository.findAll();
+  //then
 
 }
